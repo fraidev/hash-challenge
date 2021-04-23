62365880a6718d69f72032029be025fb7eb1abed
Felipe Cardozo <fraifelipe@gmail.com>
2021-04-23 19:19:45 -0300

    plural table names to singular

--- sql/init.sql
+++ sql/init.sql
@@ -1,13 +1,13 @@
-DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS product;
 
-CREATE TABLE products (
+CREATE TABLE product (
   id UUID NOT NULL PRIMARY KEY,
   price_in_cents int NOT NULL,
   title varchar(100) NOT NULL,
   description varchar(255) DEFAULT NULL
 );
 
-INSERT INTO products (id, price_in_cents, title, description)
+INSERT INTO product (id, price_in_cents, title, description)
 values 
 	('f0b19f39-b0a8-4b39-a42e-6d83f5cbd4aa', 470000,'Playsation 5','Sony video game'),
 	('827bd2cc-9537-4330-ad2b-40885344b71a', 500000,'Xbox One X','Microsoft video game'),
@@ -15,16 +15,16 @@ values
 	('3d2d1916-e11c-4b87-a514-97b082632b0b', 500000,'PC','A Personal Computer');
 
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS "user";
 
-CREATE TABLE users (
+CREATE TABLE "user" (
   id UUID NOT NULL PRIMARY KEY,
   first_name varchar(50) NOT NULL,
   last_name varchar(50) NOT NULL,
   date_of_birth date NOT NULL
 );
 
-INSERT INTO users (id, first_name, last_name, date_of_birth)
+INSERT INTO "user" (id, first_name, last_name, date_of_birth)
 VALUES
 	('9f4abbe9-113c-4099-8bef-2184d030f2c3','Felipe','Cardozo', now()),
 	('96079cd0-73a7-480a-a8bb-b6bffa0aedb7','Foo','Bar','2001-11-22'),
