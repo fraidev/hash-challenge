c98548aaac4bd02892a5479956d08a7d1ad548b7
Felipe Cardozo <fraifelipe@gmail.com>
2021-04-23 00:40:51 -0300

    discount proto

--- backend/discounts/main.go
+++ backend/discounts/main.go
@@ -1,22 +1,3 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-// Package main implements a server for Greeter service.
 package main
 
 import (
@@ -24,8 +5,8 @@ import (
 	"log"
 	"net"
 
+	pb "github.com/fraifelipe/hash-challenge/backend/discounts/protos/discount"
 	"google.golang.org/grpc"
-	pb "github.com/fraifelipe/hash-challenge/backend/discounts/protos/hello"
 )
 
 const (
@@ -34,13 +15,21 @@ const (
 
 // server is used to implement helloworld.GreeterServer.
 type server struct {
-	pb.UnimplementedGreeterServer
+	pb.UnimplementedDiscountServiceServer
 }
 
-// SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	log.Printf("Received: %v", in.GetName())
-	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+func (s *server) GetDiscount(ctx context.Context, in *pb.DiscountRequest) (*pb.DiscountReply, error) {
+	log.Printf("Received UserId: %v", in.GetUserId())
+
+	var products []*pb.ProductDiscount
+
+	for _, product := range in.Products {
+		log.Printf("Received ProductId: %v", product.Id)
+		p := pb.ProductDiscount { ProductId: product.Id, ValueInCents: 1, Percentage: 5.0 };
+		products = append(products, &p)
+	}
+
+	return &pb.DiscountReply{Products: products}, nil
 }
 
 func main() {
@@ -49,7 +38,7 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
+	pb.RegisterDiscountServiceServer(s, &server{})
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
--- backend/products/src/index.ts
+++ backend/products/src/index.ts
@@ -1,46 +1,29 @@
-// var PROTO_PATH = __dirname + '/../../../shared/protos/gerated/';
-import { GreeterClient } from '../protos/hello_grpc_pb'
-import { HelloRequest } from '../protos/hello_pb'
+import { DiscountServiceClient } from '../protos/discount_grpc_pb'
+import { DiscountRequest, Product, DiscountReply } from '../protos/discount_pb'
 import { credentials, ServiceError } from '@grpc/grpc-js';
 
-
-// let argv = 'world';
-// if (process.argv.length >= 3) {
-//     [, , argv] = process.argv;
-// }
-
-// const param: HelloRequest = new HelloRequest();
-// param.setName(argv);
-// param.setParamStruct(Struct.fromJavaScript({ foo: 'bar', bar: 'foo' }));
-// param.setParamListValue(ListValue.fromJavaScript([{ foo: 'bar' }, { bar: 'foo' }]));
-// param.setParamValue(Value.fromJavaScript('Any Value'));
-
 async function main() {
-    // var argv = parseArgs(process.argv.slice(2), {
-    //     string: 'target'
-    // });
-    // var target;
-    // if (argv.target) {
-    //     target = argv.target;
-    // } else {
-    //     target = 'localhost:50051';
-    // }
-    var client = new GreeterClient('localhost:50051', credentials.createInsecure());
-    var request = new HelloRequest();
-    var user = 'world';
-    request.setName(user);
-    client.sayHello(request, function (err: any, response: { getMessage: () => any; }) {
-        console.log('Greeting:', response.getMessage());
+    const client = new DiscountServiceClient('localhost:50051', credentials.createInsecure());
+
+    const product = new Product();
+    product.setId("155fb96c-9342-4767-8b04-c85e782cb584")
+
+    const request = new DiscountRequest();
+    request.setUserId('a1869b98-167c-43d0-9b15-5c8d29c68890');
+
+    request.setProductsList([product]);
+
+    client.getDiscount(request, function (err: ServiceError | null, response: DiscountReply) {
+        if (err) {
+            console.error(err);
+        }
+
+        const products = response.getProductsList().map(p => p.toObject());
+
+        products.forEach(product => {
+            console.log('Product Id:', product.productId, 'Percentage:', product.percentage, 'Value In Cents:', product.valueInCents);
+        })
     });
-
-
-    // client.sayHello(param, (err, res) => {
-    //     if (err) {
-    //         return console.error('sayBasic:', err.message);
-    //     }
-
-    //     console.info('sayBasic:', res.getMessage());
-    // });
 }
 
 (async (): Promise<void> => {
